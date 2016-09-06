613c45dc04ce35e1cbc60d3b5b276b93e7ad6c7e
yxgtianya <yxgtianya@163.com>
2016-09-06 18:17:44 +0800

    update and add apk

--- apptest.py
+++ apptest.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 import unittest
 from appium import webdriver
+import time
 
 class Apptest(unittest.TestCase):
 	def setUp(self):
@@ -7,9 +9,9 @@ class Apptest(unittest.TestCase):
 		desired_caps['platformName'] = 'Android'
 		desired_caps['platformVersion'] = "6.0.1"
 		desired_caps['deviceName'] = 'Nexus6'
-		#desired_caps['app'] = PATH('')
-		desired_caps['appPackage'] = 'cn.buding.martin'
-		desired_caps['appActivity'] = '.activity.SplashActivity'
+		desired_caps['app'] = ('C:\Users\yxg\Desktop\\appTest\helloworld\\test.apk')
+		#desired_caps['appPackage'] = 'cn.buding.martin'
+		#desired_caps['appActivity'] = '.activity.SplashActivity'
 
 		self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
@@ -20,10 +22,21 @@ class Apptest(unittest.TestCase):
 		self.driver.remove_app('io.appium.android.im')
 		self.driver.quit()
 
-	def test_1():
-		driver.sleep(2000)
-		driver.swipe(start_x=1300, start_y=1200, end_x =100, end_y=1200, duration=800)
-		driver.swipe(start_x=1300, start_y=1200, end_x =100, end_y=1200, duration=800)
+	def test_1(self):
+		#driver.
+		#self.driver.reset()
+		#self.driver.launch_app()
+		print(self.driver.get_window_size())
+		time.sleep(4)
+		self.driver.swipe(start_x=1000, start_y=1200, end_x =500, end_y=1200, duration=500)
+		time.sleep(1)
+		self.driver.swipe(start_x=1000, start_y=1200, end_x =500, end_y=1200, duration=500)
+		#print(self.driver.current_activity())
+		time.sleep(1)
+		start = self.driver.find_element_by_id('cn.buding.martin:id/start')
+		start.click()
+		time.sleep(1)
+		print self.driver.current_activity
 
 if __name__== '__main__':
 	#suite = unittest.TestLoader().loadTestsFromTestCase(ContactsAndroidTests)
