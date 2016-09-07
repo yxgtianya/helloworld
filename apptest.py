53906e956563cdb7fa1372bbde56a2b1cab3d226
yxgtianya <yxgtianya@163.com>
2016-09-07 12:46:04 +0800

    add suite

--- apptest.py
+++ apptest.py
@@ -9,35 +9,38 @@ class Apptest(unittest.TestCase):
 		desired_caps['platformName'] = 'Android'
 		desired_caps['platformVersion'] = "6.0.1"
 		desired_caps['deviceName'] = 'Nexus6'
+		desired_caps['noSign'] = 'true'
 		desired_caps['app'] = ('C:\Users\yxg\Desktop\\appTest\helloworld\\test.apk')
+		desired_caps['fullReset'] = 'true'
+		#desired_caps['unicodeKeyboard'] = 'true'
 		#desired_caps['appPackage'] = 'cn.buding.martin'
 		#desired_caps['appActivity'] = '.activity.SplashActivity'
 
 		self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
 	def tearDown(self):
-		self.driver.close_app()
+		#self.driver.close_app()
 		self.driver.remove_app('io.appium.unlock')
 		self.driver.remove_app('io.appium.settings')
 		self.driver.remove_app('io.appium.android.im')
 		self.driver.quit()
 
 	def test_1(self):
-		#driver.
-		#self.driver.reset()
-		#self.driver.launch_app()
 		print(self.driver.get_window_size())
 		time.sleep(4)
 		self.driver.swipe(start_x=1000, start_y=1200, end_x =500, end_y=1200, duration=500)
 		time.sleep(1)
 		self.driver.swipe(start_x=1000, start_y=1200, end_x =500, end_y=1200, duration=500)
-		#print(self.driver.current_activity())
 		time.sleep(1)
 		start = self.driver.find_element_by_id('cn.buding.martin:id/start')
 		start.click()
 		time.sleep(1)
 		print self.driver.current_activity
 
+def suite():
+	suite = unittest.TestSuite()
+	suite.addTest(Apptest("test_1"))
+	return suite
+
 if __name__== '__main__':
-	#suite = unittest.TestLoader().loadTestsFromTestCase(ContactsAndroidTests)
-    unittest.main()
+	unittest.main(defaultTest = 'suite')
