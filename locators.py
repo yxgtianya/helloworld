ace13c885a1fa7b9925c520707fa30f2cfd593e4
yxgtianya <yxgtianya@163.com>
2016-09-13 19:25:43 +0800

    encoding fixed

--- locators.py
+++ locators.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from selenium.webdriver.common.by import By
 
 class BeginPageLocators(object):
--- page.py
+++ page.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from element import BasePageElement
 from locators import BeginPageLocators
 
