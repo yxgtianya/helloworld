9a402600774dfbc297b311727fbec10d0551808f
yxgtianya <yxgtianya@163.com>
2016-09-14 17:37:16 +0800

    add send email function

--- apptest.py
+++ apptest.py
@@ -2,55 +2,67 @@
 import unittest
 from appium import webdriver
 import page
-import time
+from time import sleep
 import HTMLTestRunner
+import send_email 
 
 class Apptest(unittest.TestCase):
+	desired_caps = {}
+	desired_caps['platformName'] = 'Android'
+	desired_caps['platformVersion'] = "6.0.1"
+	desired_caps['deviceName'] = 'Nexus6'
+	#不重新签名
+	desired_caps['noSign'] = 'true'
+	desired_caps['app'] = 'C:\Users\yxg\Desktop\\appTest\helloworld\\test.apk'
+	desired_caps['newCommandTimeout'] = '30'
+	desired_caps['noReset'] = 'true'
+	#测试结束卸载app
+	#desired_caps['fullReset'] = 'true'
+	#desired_caps['unicodeKeyboard'] = 'true'
+	desired_caps['appPackage'] = 'cn.buding.martin'
+	desired_caps['appActivity'] = '.activity.SplashActivity'
+
+	driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+		
+
 	def setUp(self):
-		desired_caps = {}
-		desired_caps['platformName'] = 'Android'
-		desired_caps['platformVersion'] = "6.0.1"
-		desired_caps['deviceName'] = 'Nexus6'
-		#不重新签名
-		desired_caps['noSign'] = 'true'
-		desired_caps['app'] = ('C:\Users\yxg\Desktop\\appTest\helloworld\\test.apk')
-		#测试结束卸载app
-		desired_caps['fullReset'] = 'true'
-		#desired_caps['unicodeKeyboard'] = 'true'
-		#desired_caps['appPackage'] = 'cn.buding.martin'
-		#desired_caps['appActivity'] = '.activity.SplashActivity'
-
-		self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
+		print 'setup'
+		
 	def tearDown(self):
-		#卸载安装的app
-		self.driver.remove_app('io.appium.unlock')
-		self.driver.remove_app('io.appium.settings')
-		self.driver.remove_app('io.appium.android.im')
-		self.driver.quit()
+		print 'tearDown'
 
 	def test_1(self):
 		print(self.driver.get_window_size())
-		time.sleep(4)
+		sleep(4)
 		link_page = page.LinkPage(self.driver)
 		link_page.swipertl()
-		time.sleep(1)
+		sleep(1)
 		link_page.swipertl()
-		time.sleep(1)
+		sleep(1)
+		
+	def test_2(self):
 		begin_page = page.BeginPage(self.driver)
 		begin_page.click_begin_button()
-		time.sleep(1)
+		sleep(1)
 		print self.driver.current_activity
-
-def suite():
-	suite = unittest.TestSuite()
-	suite.addTest(Apptest("test_1"))
-	return suite
-
+		
+	def test_3(self):
+	    #卸载安装的app
+		self.driver.remove_app('io.appium.unlock')
+		self.driver.remove_app('io.appium.settings')
+		self.driver.remove_app('io.appium.android.im')
+		self.driver.remove_app('cn.buding.martin')
+		
+	
+		
 if __name__== '__main__':
+	suite = unittest.TestLoader().loadTestsFromTestCase(Apptest)
+	#unittest.TextTestRunner(verbosity=2).run(suite)
 	filename = 'd:\\result.html'
 	fp = file(filename, 'wb')
 	runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='Test result of cn.buding.martin', 
 	description='Test_Report')
 	#unittest.main(defaultTest = 'suite')]
-	runner.run(suite())
+	runner.run(suite)
+	Apptest.driver.quit()
+	send_email.send_report()
---
+++ send_email.py
@@ -0,0 +1,50 @@
+#coding:utf-8
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+
+mail_host = 'smtp.163.com'
+mail_user = 'yxgtianya@163.com'
+mail_pass = '#dzw**' 
+
+sender = 'yxgtianya@163.com'
+receivers = ['yuxuguang@weiche.cn']
+
+message = MIMEMultipart()
+#message = MIMEText('附件为测试报告', 'plain', 'utf-8')
+h = Header('yxgtinaya@163.com', 'utf-8')
+h.append('<yxgtianya@163.com>', 'ascii')
+message["From"] = h
+message['To'] =  'yuxuguang@weiche.cn'
+
+subject = 'Test_report'
+message['Subject'] = Header(subject, 'utf-8')
+
+file_open = open('d:\\result.html')
+try:
+	all_the_html = file_open.read()
+#	print all_the_html
+finally:
+	file_open.close()
+	
+message_html = all_the_html
+message.attach(MIMEText(message_html, 'html', 'utf-8'))
+
+att1 = MIMEText(open('d:\\result.html', 'rb').read(), 'base64', 'utf-8')
+att1["Content-Type"] = 'application/octet-stream'
+att1["Content-Disposition"] = 'attachment; filename="result.html"'
+att1["Content-Disposition"] = 'attachment; filename="result.html"'
+message.attach(att1)
+
+def send_report():
+	try:
+		smtpObj = smtplib.SMTP()
+		smtpObj.connect(mail_host)
+		smtpObj.login(mail_user, mail_pass)
+		smtpObj.sendmail(sender, receivers, message.as_string())
+		print 'send mail sucessed'
+	except smtplib.SMTPException:
+		print 'send mail failed'
+		
+	
